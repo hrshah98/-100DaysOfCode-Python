254f1b06b11911828a53ab13dab05008679b1f48
hrshah98 <89706705+hrshah98@users.noreply.github.com>
2021-08-29 01:57:53 +0530

    Band Name Generator Program
    A funny program that'll come up with a name of your next band.

---
+++ Day - 1/main.py
@@ -0,0 +1,9 @@
+# Band Name Generator Program
+
+print("Welcome to the Band Name Generator.")
+
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+combined_string = city_name + " " + pet_name
+
+print("Your band name could be " + combined_string)
